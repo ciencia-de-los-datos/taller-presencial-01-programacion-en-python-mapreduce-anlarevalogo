e80a2e5f161b09a18c99c1633ed9b177ad9374c4
anlarevalogo <160282139+anlarevalogo@users.noreply.github.com>
2024-02-23 20:25:01 -0500

    Add files via upload

--- word_count.py
+++ word_count.py
@@ -1,10 +1,9 @@
-#
-# Escriba la función load_input que recive como parámetro un folder y retorna
+# Esciba la función load_input que recibe como parámetro un folder y retorna
 # una lista de tuplas donde el primer elemento de cada tupla es el nombre del
 # archivo y el segundo es una línea del archivo. La función convierte a tuplas
 # todas las lineas de cada uno de los archivos. La función es genérica y debe
 # leer todos los archivos de folder entregado como parámetro.
-#
+
 # Por ejemplo:
 #   [
 #     ('text0'.txt', 'Analytics is the discovery, inter ...'),
@@ -13,8 +12,23 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
+
+import glob
+import os.path
+import fileinput
+
 def load_input(input_directory):
-    pass
+
+    sequence = []
+    filenames = glob.glob(input_directory + "/*")
+    with fileinput.input(files=filenames) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    
+    return sequence
+
+filenames = load_input("input")
+print(filenames)
 
 
 #
@@ -28,9 +42,19 @@ def load_input(input_directory):
 #     ('is', 1),
 #     ...
 #   ]
-#
+#Incluir limpieza de datos
+
 def mapper(sequence):
-    pass
+
+    new_sequence = []    
+    for _, text in sequence:
+        words = text.split()
+        for word in words:
+            word = word.replace(",","")
+            word = word.replace(".","")
+            word = word.lower()
+            new_sequence.append((word,1))
+    return new_sequence
 
 
 #
@@ -45,7 +69,8 @@ def mapper(sequence):
 #   ]
 #
 def shuffle_and_sort(sequence):
-    pass
+    sorted_sequence = sorted(sequence, key=lambda x: x[0])
+    return sorted_sequence
 
 
 #
@@ -53,17 +78,48 @@ def shuffle_and_sort(sequence):
 # reduce los valores asociados a cada clave sumandolos. Como resultado, por
 # ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
 # texto.
-#
+# Como se procesan elementos de la misma clave ejemplo (palabra, # veces) (analytics, 4)
+# En este punto el codigo ya reconoce la clave y el valor 
+
 def reducer(sequence):
-    pass
+
+    diccionario = {}
+    '''for key, value in sequence:
+        if key not in diccionario.keys():
+            #Primero crear llave antes de llenarla
+            diccionario[key] = []  
+        diccionario[key].append(value)
+    print(diccionario)
+
+    sequence = []
+    for key, value in diccionario.items():
+        tuple = (key, sum(value))
+        sequence.append(tuple)'''
+
+    for key, value in sequence:
+        if key not in diccionario.keys():
+            #Primero crear llave antes de llenarla
+            diccionario[key] = 0  
+        diccionario[key] += value
+    print(diccionario)
+
+    sequence = []
+    for key, value in diccionario.items():
+        tuple = (key, value)
+        sequence.append(tuple)
+
+    return sequence
 
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
+
 def create_ouptput_directory(output_directory):
-    pass
+    if os.path.exists(output_directory):
+        raise FileExistsError(f"La carpeta '{output_directory}' ya existe")
+    os.makedirs(output_directory)
 
 
 #
@@ -75,7 +131,9 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    with open(output_directory + "/part-0000", "w") as file:
+        for key, value in sequence:
+            file.write(f"{key}\t{value}\n")
 
 
 #
@@ -83,18 +141,25 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
+    with open(output_directory + "/_success", "w") as file:
+        file.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
 
 if __name__ == "__main__":
     job(
         "input",
-        "output",
+        "output"
     )
